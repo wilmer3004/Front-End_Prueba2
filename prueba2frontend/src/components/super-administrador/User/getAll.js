dd4f3e2dfd15de651cb2eab70649de73643b331c
Yeison123456 <yeisonrodrigueznacun@gmail.com>
2024-01-12 13:18:41 -0500

    save change

---
+++ prueba2frontend/src/components/super-administrador/User/getAll.js
@@ -0,0 +1,51 @@
+import Cookies from "js-cookie";
+import React, { useState, useEffect } from 'react';
+
+const apiURL = '/api/user';
+
+const GetAll = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const handleGetAll = async () => {
+      try {
+        const authToken = Cookies.get('authToken');
+        const headers = {
+          'Authorization': `Bearer ${authToken}`
+        };
+
+        if (authToken) {
+          headers.Authorization = `Bearer ${authToken}`;
+        }
+
+        const response = await fetch(apiURL, { headers });
+
+        if (!response.ok) {
+          throw new Error(`Request failed with status: ${response.status}`);
+        }
+
+        const responseData = await response.json();
+        setData(responseData);
+        console.log(responseData);
+      } catch (error) {
+        console.error('Request failed:', error.message);
+        throw error;
+      }
+    };
+
+    handleGetAll();
+  }, []); // Ejecuta la función al montar el componente
+
+  return (
+    <div>
+      <h1>Mis Datos</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.idUsuario}>{item.primerNombre} - {item.primerApellido}- {item.id_tipo_docfk.nombreTipoDoc}-{item.username}-</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default GetAll;
