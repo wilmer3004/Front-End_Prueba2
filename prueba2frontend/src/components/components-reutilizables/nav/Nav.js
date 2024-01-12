a76e53be6c0bbd107dbd7d6bb5cb49cbba816171
wilmer3004 <wilmerandres3004@gmail.com>
2024-01-12 14:17:08 -0500

    Create Nav.js

---
+++ prueba2frontend/src/components/components-reutilizables/nav/Nav.js
@@ -0,0 +1,30 @@
+// Nav.js
+
+import React from 'react';
+import "./Nav.css"
+import { Link } from 'react-router-dom';
+
+const Nav = ({ items }) => {
+    return (
+        <nav>
+            <div className={"title"}>
+                <h1>Accesorios de computadores</h1>
+            </div>
+            <div className={"botones"}>
+                <ul>
+                    {items.map((item, index) => (
+                        <li key={index}>
+                            <Link to={`/${item.toLowerCase()}`} className={"data-button"}>
+                                <button>
+                                    {item}
+                                </button>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
