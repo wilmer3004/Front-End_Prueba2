659be7e40cb6d4fba41bc2893f267efb3320fdce
wilmer3004 <wilmerandres3004@gmail.com>
2024-01-12 10:21:37 -0500

    save changes

---
+++ prueba2frontend/src/components/usuario-regular/usuario-reular-page/UsuarioRegularPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UsuarioRegularPage = ()=>{
+    return <div>
+        <p>Soy la pagina de usuario regular</p>
+    </div>;
+};
+
+export default UsuarioRegularPage
+
+
+
