e6d00e5ec51ba89d6b61a463ac0cc717026cfa95
wilmer3004 <wilmerandres3004@gmail.com>
2024-01-16 14:55:48 -0500

    save changes rol security

--- prueba2frontend/src/App.js
+++ prueba2frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/login/Login";
 import AdministradorPage from "./components/administrador/administrador-page/AdministradorPage";
-import SuperAdministradorPage from "./components/super-administrador/super-administrador-page/SuperAdministradorPage";
+import SuperAdministradorPage from "./components/super-administrador/super-administrador-page/layouts/SuperAdministradorPage";
 import UsuarioRegularPage from "./components/usuario-regular/usuario-reular-page/UsuarioRegularPage";
 import GetAll from "./components/super-administrador/User/layouts/getAll";
 import ServiciosPage from "./components/super-administrador/servicios/layout/ServiciosPage";
---
+++ prueba2frontend/src/api/Auth.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useState, useEffect } from 'react';
+
+const apiURL = '/api/user/loginAuth';
+
+const AuthData = () => {
+    const [responseState, setResponseState] = useState({
+        data: null,
+        status: null,
+        error: null,
+        loading: true
+    });
+
+    const fetchData = async () => {
+
+        try {
+            const authToken = Cookies.get('authToken');
+            const headers = {
+                'Authorization': `Bearer ${authToken}`
+            };
+
+            const response = await axios.get(apiURL, { headers });
+
+            setResponseState({
+                data: response.data,
+                status: response.status,
+                error: null,
+                loading: false
+            });
+        } catch (error) {
+            let errorData = error.message;
+            if (errorData.includes("403")) {
+                setResponseState({
+                    data: null,
+                    status: 403,
+                    error: error.message,
+                    loading: false
+                });
+                Cookies.remove('authToken');
+                window.location.href = '/'
+
+            } else {
+                setResponseState({
+                    data: null,
+                    status: error.response ? error.response.status : null,
+                    error: error.message,
+                    loading: false
+                });
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return {responseState};
+};
+
+export default AuthData;
